7b51db7c1fdea82daf61af5528ed4f51fb253b1d
Zeev Rotshtein <zeevro@gmail.com>
2020-11-05 15:20:27 +0200

    Remove setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
 from setuptools import setup
 
 
-setup()
+setup(
+    name='comports',
+    version='0.1.0',
+    url='https://github.com/zeevro/comports',
+    download_url='https://github.com/zeevro/comports/archive/master.zip',
+    author='Zeev Rotshtein',
+    author_email='zeevro@gmail.com',
+    maintainer='Zeev Rotshtein',
+    maintainer_email='zeevro@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    license=None,
+    description='A small GUI for launching PuTTY for COM ports',
+    keywords=[
+        'PuTTY',
+        'COM',
+        'serial',
+        'GUI',
+    ],
+    zip_safe=True,
+    packages=[
+        'comports',
+    ],
+    install_requires=[
+        'pyserial',
+    ],
+    entry_points=dict(
+        gui_scripts=[
+            'comports = comports.gui:main',
+        ],
+    ),
+)
